16c476783d3fbcecc5955e5749d319746bb04e4d
tapp41k <icepeak5@yandex.ru>
2023-05-07 16:26:17 +0300

    bugfix_v2

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -6,17 +6,17 @@ from .views import CommentViewSet, GroupViewSet, FollowViewSet, PostViewSet
 
 app_name = 'api'
 
-router = routers.SimpleRouter()
-router.register(
+router_v1 = routers.SimpleRouter()
+router_v1.register(
     r'^posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
-router.register(r'groups', GroupViewSet, basename='groups')
-router.register(r'follow', FollowViewSet, basename='follow')
-router.register(r'posts', PostViewSet, basename='posts')
+router_v1.register(r'groups', GroupViewSet, basename='groups')
+router_v1.register(r'follow', FollowViewSet, basename='follow')
+router_v1.register(r'posts', PostViewSet, basename='posts')
 
 urlpatterns = [
-    path('v1/', include(router.urls,)),
+    path('v1/', include(router_v1.urls,)),
     path('v1/', include('djoser.urls.jwt')),
 ]
